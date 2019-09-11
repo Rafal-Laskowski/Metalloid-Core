d41e06278b8b3cbcc0ddd03388cefeda1f6c86ae
rafal_laskowski <rafal.laskowski@intive.com>
2019-09-11 09:18:29 +0200

    Refactoring

--- src/com/github/metalloid/core/Metalloid.java
+++ src/com/github/metalloid/core/Metalloid.java
@@ -1,10 +1,25 @@
 package com.github.metalloid.core;
 
+import com.github.metalloid.pagefactory.PageFactory;
+import com.github.metalloid.webdriver.WebDriverPool;
+import com.github.metalloid.webdriver.utils.UtilsFactory;
+import com.github.metalloid.webdriver.utils.conditions.ConditionEvaluator;
+import org.openqa.selenium.WebDriver;
+
 public class Metalloid {
 
+    public static void initializePage(WebDriver driver, Object page) {
+        PageFactory.init(driver, page);
+        UtilsFactory.initUtilities(driver, page);
+        ConditionEvaluator.evaluate(driver, page);
+    }
+
+    /**
+     * Use this method only if you use WebDriver-Pool library of Metalloid
+     * @param page
+     */
     public static void initializePage(Object page) {
-        PageFactory.init(page);
-        UtilsFactory.initUtilities(page);
-        ConditionEvaluator.evaluate(page);
+        WebDriver driver = WebDriverPool.get();
+        initializePage(driver, page);
     }
 }
